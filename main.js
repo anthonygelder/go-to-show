eeaefb26049ab90224fdb803380360ac997ce825
Anthony Gelder <anthony.gelder@gmail.com>
2019-08-06 19:10:18 -0700

    mvp-ish

--- main.js
+++ main.js
@@ -1,38 +1,25 @@
 function generateHTML(obj) {
     return `
         <div class="event">
-            <p>${obj.name}</p>
+            <p>${obj.displayName}</p>
         </div>
     `
 }
 
 function processData(responseData) {
-    // console.log(responseData._embedded.events[0].classifications[0].segment.name);
 
-
-
-    const showDataHtml = responseData._embedded.events.map(function(obj) {
-        // console.log(responseData._embedded.events[0].classifications[0].segment.name);
-        const type = responseData._embedded.events[0].classifications[0].segment.name;
-        console.log(type);
-        // if (type === "Music") {
-            return generateHTML(obj);
-        // }
+    const showDataHtml = responseData.resultsPage.results.event.map(function(obj) {
+        // console.log(responseData.resultsPage.results.event);
+        return generateHTML(obj);
     }).join('');
+
 
     $('#results').html(showDataHtml);
 }
 
-function getEvents(city, zip) {
-    let apiUrl = ``;
-    if (zip == '') {
-        
-        apiUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=CTzeAwSDWbUNHqIaEVP90VJry8rUWbOZ&city=${city}`;
-    } else if ( city == '') {
-        console.log("here");
-        apiUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=CTzeAwSDWbUNHqIaEVP90VJry8rUWbOZ&postalCode=${zip}`;
-    } 
-    console.log(apiUrl);
+function getEvents(id) {
+    const apiUrl = `https://api.songkick.com/api/3.0/metro_areas/${id}/calendar.json?apikey=lKGlBIRmnawI3yka`;
+
     fetch(apiUrl)
         .then(response => response.json())
         .then(responseJson => processData(responseJson));
@@ -41,15 +28,25 @@ function getEvents(city, zip) {
 }
 
 
+function getCityId(city) {
+    const apiUrl = `https://api.songkick.com/api/3.0/search/locations.json?query=${city}&apikey=lKGlBIRmnawI3yka`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(responseJson => getEvents(responseJson.resultsPage.results.location[0].metroArea.id));
+}
+
 
 function main() {
     $('form').on('submit', function(event) {
         event.preventDefault();
         const city = $(event.currentTarget).find('#city-search').val();
-        const zip = $(event.currentTarget).find('#zip-search').val();
-        console.log(city, zip);
-        getEvents(city, zip);
+        // const zip = $(event.currentTarget).find('#zip-search').val();
+        // console.log(city, zip);
+        getCityId(city);
     })
 }
 
 $(main)
+
+// ekgXaC6NfwJzIke8BGqob64gLjr4Wl
